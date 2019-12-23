2b5b35398da97d2799eb8725b452f2b64a86a2ec
jj <852649268@qq.com>
2019-12-23 17:21:33 +0800

    udaf多列测试

--- spark-sql/src/main/scala/com/jj/spark/sql/UDAFWithSqlTest.scala
+++ spark-sql/src/main/scala/com/jj/spark/sql/UDAFWithSqlTest.scala
@@ -14,31 +14,36 @@ import org.apache.spark.sql.types.{DataType, DataTypes, StructField, StructType}
   */
 class MyAverageWithSql extends UserDefinedAggregateFunction {
 
-  override def inputSchema: StructType = StructType(StructField("salary",DataTypes.IntegerType)::Nil)
+  override def inputSchema: StructType = StructType(StructField("salary",DataTypes.IntegerType)::StructField("name",DataTypes.StringType)::Nil)
 
-  override def bufferSchema: StructType = StructType(StructField("sum",DataTypes.LongType)::StructField("salary",DataTypes.IntegerType)::Nil)
+  override def bufferSchema: StructType = StructType(StructField("sum",DataTypes.LongType)::StructField("count",DataTypes.IntegerType)::StructField("name",DataTypes.StringType)::Nil)
 
-  override def dataType: DataType = DataTypes.DoubleType
+  override def dataType: DataType = DataTypes.StringType
 
   override def deterministic: Boolean = true
 
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
     buffer(0)=0L
     buffer(1)=0
+    buffer(2)=""
   }
 
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
     buffer(0) = buffer.getLong(0)+input.getInt(0)
     buffer(1)=buffer.getInt(1)+1
+    val name = if(buffer.getString(2)=="")input.getString(1) else buffer.getString(2)+","+input.getString(1)
+    buffer(2)=name
   }
 
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
     buffer1(0) = buffer1.getLong(0)+buffer2.getLong(0)
     buffer1(1) = buffer1.getInt(1)+buffer2.getInt(1)
+    val name = if(buffer1.getString(2)=="")buffer2.getString(2) else buffer1.getString(2)+","+buffer2.getString(2)
+    buffer1(2) = name
   }
 
   override def evaluate(buffer: Row): Any = {
-    buffer.getLong(0).toDouble / buffer.getInt(1)
+    buffer.getLong(0).toDouble / buffer.getInt(1)+"->"+buffer.getString(2)
   }
 }
 
@@ -49,16 +54,16 @@ object UDAFWithSqlTest {
   import spark.implicits._
 
   def UDAFWithSql: Unit ={
-    val df = spark.read.json("H:\\bigdata-dev\\ideaworkspace\\tanzhou\\spark\\spark-sql\\src\\main\\resources\\employees.json")
+    val df = spark.read.json("file:\\H:\\bigdata-dev\\ideaworkspace\\tanzhou\\spark\\spark-sql\\src\\main\\resources\\employees.json")
     df.createOrReplaceTempView("emp")
     //缓存中间表，用于后面多次操作,也是懒加载的，第一次执行action触发
     spark.sqlContext.cacheTable("emp")
     df.withColumn("salary",$"salary".cast(DataTypes.IntegerType))
     //注意注册的UDAF对象不能嵌套三层内部类，最好放在外部
     spark.udf.register("average",new MyAverageWithSql)
-    val frame = spark.sql("select average(salary) avg from emp")
-    println(frame.collect().map(_.getDouble(0)).head)
-    frame.show()
+    val frame = spark.sql("select average(salary,name) avg from emp")
+//    println(frame.collect().map(_.getDouble(0)).head)
+    frame.show(false)
     //中间表使用完毕需要清除缓存
     spark.sqlContext.uncacheTable("emp")
     //清除所有缓存
